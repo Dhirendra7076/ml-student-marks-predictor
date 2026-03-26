0758085a71f5fa91835f4b4a0fae49337a2b6f1a
Dhirendra Singh <dhirendrasinghkv2@gmail.com>
2026-03-26 17:03:33 +0530

    fix: ADDED CLIENT  URLs

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "https://ml-student-marks-predictor.vercel.app",
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 app.use(express.json());
