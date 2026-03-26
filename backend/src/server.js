a6befb59a5b87f2ad41228ee5da285751701b5f1
Dhirendra Singh <dhirendrasinghkv2@gmail.com>
2026-03-26 17:20:43 +0530

    fix: cors preflight properly configured

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,20 +13,19 @@ connectDB();
 const app = express();
 
 
-app.use(cors({
+const corsOptions = {
   origin: "https://ml-student-marks-predictor.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.options("*", cors());
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieparser());
 app.use("/api/auth" , authRoutes)
 
 const PORT = process.env.PORT ||5000
-await mongoose.connect(process.env.MONGO_URI)
-
 //routes
 app.use("/api" , predictRoutes)
 
