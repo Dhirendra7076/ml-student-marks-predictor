a9e5407e61ac3cdb2d3d43baa4a63fc4feec0caf
Dhirendra Singh <dhirendrasinghkv2@gmail.com>
2026-03-26 16:05:00 +0530

    1

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom"
-import { Home, History } from "lucide-react"
-import { useState } from "react"
 
-
-
-  function Sidebar() {
-    const [isOpen , setIsOpenN] = useState(true)
+  function Sidebar({ isOpen }) {
   return (
-    <div className="w-60 bg-base-200 border-r border-base-300 p-4">
-
+    <div
+      className={`
+        fixed top-0 left-0 h-full w-60 
+        bg-base-200 border-r border-base-300 p-4
+        transform transition-transform duration-300 z-50
+        ${isOpen && (
+  <div
+    className="fixed inset-0 bg-black/30 z-40"
+    onClick={() => setIsOpen(false)}
+  />
+)}`}
+    >
       <ul className="menu text-base-content">
-
         <li>
           <Link to="/">Predictor</Link>
         </li>
@@ -18,9 +22,7 @@ import { useState } from "react"
         <li>
           <Link to="/history">History</Link>
         </li>
-
       </ul>
-
     </div>
   )
 }
